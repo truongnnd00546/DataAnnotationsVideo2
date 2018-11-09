ec64f5f3778cc56a06942962f06542ee22636cc5
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-11-09 12:06:10 +0700

    Success

---
+++ DataAnnotations/Context/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DataAnnotations.Models;
+
+namespace DataAnnotations.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
